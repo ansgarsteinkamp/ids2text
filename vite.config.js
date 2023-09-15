af9227cd2ee31ceefdda7ebccfe7673e5969bb95
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-09-15 20:13:17 +0200

    base ergänzt

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-   server: { open: true }
+   server: { open: true },
+   base: "/ids2text/"
 });
